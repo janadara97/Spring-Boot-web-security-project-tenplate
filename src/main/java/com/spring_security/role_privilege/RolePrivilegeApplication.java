585d7c986bec6f77d3f45e0b984277042f6f7741
janadara97 <chathurajanadara97@gmail.com>
2021-08-11 17:31:21 +0530

    Spring Boot Web Security project template

---
+++ src/main/java/com/spring_security/role_privilege/RolePrivilegeApplication.java
@@ -0,0 +1,13 @@
+package com.spring_security.role_privilege;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RolePrivilegeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RolePrivilegeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring_security/role_privilege/RolePrivilegeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring_security.role_privilege;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RolePrivilegeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
